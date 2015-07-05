72cbbbd90f9780845ce64011f11699fc200f3e19
Haythem Khiri <haythem512@live.fr>
2015-07-05 20:23:50 +0200

    Update argouml-installer.sh

--- argouml-installer.sh
+++ argouml-installer.sh
@@ -1,37 +1,25 @@
 #!/bin/sh
 # sudo sh -c "echo $data >>$logfile"
 # --------------------------------------------------
-# Setup argoUML 2
+# Setup AgroUML 2
 # --------------------------------------------------
-echo "Installing argoUML..."
-sudo mv argouml /usr/lib/argoUML
-sudo ln -s /usr/lib/argoUML /usr/lib/argoUML
-sudo ln -s /usr/lib/argoUML/argouml.sh /usr/bin/argoUML
-sudo cp /usr/lib/argoUML/icob/argouml2.svg /usr/share/pixmaps/argoUML.png
-sudo sh -c 'echo "[Desktop Entry]" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Encoding=UTF-8" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Version=1.0" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Name=ArgoUML" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Comment=UML tool" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Categories=Development;" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Exec=/usr/bin/argoUML" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Icon=/usr/share/pixmaps/argoUML.png" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Terminal=false" | tee -a /usr/share/applications/argoUML.desktop'
-sudo sh -c 'echo "Type=Application" | tee -a /usr/share/applications/argoUML.desktop'
-xdg-desktop-menu install /usr/share/applications/argoUML.desktop
-tar -xvf dotfiles/.WebIde10.tar.gz
-mv .WebIde10 ~/.WebIde10
-# --------------------------------------------------
-# Setup argoUML Upgrade
-# --------------------------------------------------
-echo "Setting up upgrade argoUML..."
-mkdir ~/.local
-cp platforms/linux/upgrade-argoUML.sh ~/.local/upgrade-argoUML.sh
-echo 'alias upgrade argoUML="$HOME/.local/upgrade-argoUML.sh"' | tee -a ~/.bash_aliases
-chmod 755 ~/.local/upgrade-argoUML.sh
-if [ $?=0 ]
-then echo "argoUML: installed"
-else
-echo "argoUML: failed"
-exit 1
+echo "Installing agroUML..."
+# Create the /usr/share/pixmaps/ dir if it doesn't exist
+if [ ! -d /usr/share/pixmaps/ ]; then
+       sudo mkdir /usr/share/pixmaps/
 fi
+sudo cp /usr/bin/agroUML/bin/agroUML.png /usr/share/pixmaps/agroUML.png
+
+# Setup of the .desktop file
+sudo echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=ArgoUML
+Comment=UML tool
+Categories=Development;
+Exec=/usr/bin/argoUML/bin/agroUML.sh
+Icon=/usr/share/pixmaps/argoUML.png
+Terminal=false" >> /usr/share/applications/agroUML.desktop
+xdg-desktop-menu install /usr/share/applications/agroUML.desktop
+
+echo "AgroUML has been installed"
