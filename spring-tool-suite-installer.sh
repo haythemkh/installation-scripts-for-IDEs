11d45d0a9d10a8671a157c4b5edc47f08cfacf12
Haythem Khiri <haythem512@live.fr>
2015-07-05 20:27:42 +0200

    Update spring-tool-suite-installer.sh

--- spring-tool-suite-installer.sh
+++ spring-tool-suite-installer.sh
@@ -1,36 +1,25 @@
 #!/bin/sh
-# To execute this file, you must:
-#  * rename ./icon.xpm to spring-tool-studio.xpm
+# sudo sh -c "echo $data >>$logfile"
+# --------------------------------------------------
+# Setup spring-tool-suite 2
 # --------------------------------------------------
 echo "Installing spring-tool-suite..."
-sudo mv spring* /usr/lib/spring-tool-suite
-sudo ln -s /usr/lib/spring-tool-suite /usr/lib/spring-tool-suite
-sudo ln -s /usr/lib/spring-tool-suite/STS /usr/bin/
-sudo cp /usr/lib/spring-tool-suite/spring-tool-suite.png /usr/share/pixmaps/
-sudo sh -c 'echo "[Desktop Entry]" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Encoding=UTF-8" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Version=1.0" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Name=STS" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Comment=IDE for JavaEE/Spring development" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Categories=Development;IDE;" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Exec=/usr/bin/STS" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Icon=/usr/share/pixmaps/spring-tool-suite.png" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Terminal=false" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-sudo sh -c 'echo "Type=Application" | tee -a /usr/share/applications/spring-tool-suite.desktop'
-xdg-desktop-menu install /usr/share/applications/spring-tool-suite.desktop
-tar -xvf dotfiles/.WebIde10.tar.gz
-mv .WebIde10 ~/.WebIde10
-# --------------------------------------------------
-# Setup spring-tool-suite Upgrade
-# --------------------------------------------------
-echo "Setting up upgradespring-tool-suite..."
-mkdir ~/.local
-cp platforms/linux/upgrade-spring-tool-suite.sh ~/.local/upgrade-spring-tool-suite.sh
-echo 'alias upgradespring-tool-suite="$HOME/.local/upgrade-spring-tool-suite.sh"' | tee -a ~/.bash_aliases
-chmod 755 ~/.local/upgrade-spring-tool-suite.sh
-if [ $?=0 ]
-then echo "spring-tool-suite: installed"
-else
-echo "spring-tool-suite: failed"
-exit 1
+# Create the /usr/share/pixmaps/ dir if it doesn't exist
+if [ ! -d /usr/share/pixmaps/ ]; then
+       sudo mkdir /usr/share/pixmaps/
 fi
+sudo cp /usr/bin/spring-tool-suite/bin/spring-tool-suite.png /usr/share/pixmaps/spring-tool-suite.png
+
+# Setup of the .desktop file
+sudo echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Spring Tool Suite
+Comment=IDE for JavaEE/Spring development
+Categories=Development;
+Exec=/usr/bin/argoUML/bin/spring-tool-suite.sh
+Icon=/usr/share/pixmaps/argoUML.png
+Terminal=false" >> /usr/share/applications/spring-tool-suite.desktop
+xdg-desktop-menu install /usr/share/applications/spring-tool-suite.desktop
+
+echo "spring-tool-suite has been installed"
