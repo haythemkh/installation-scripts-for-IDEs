972985f6d5f24a65e529e3d6d33eaf7b380cfe8c
Haythem Khiri <haythem512@live.fr>
2015-07-05 20:25:55 +0200

    Update webstorm-installer.sh

--- webstorm-installer.sh
+++ webstorm-installer.sh
@@ -12,7 +12,7 @@ sudo sh -c 'echo "[Desktop Entry]" | tee -a /usr/share/applications/jetbrains-we
 sudo sh -c 'echo "Encoding=UTF-8" | tee -a /usr/share/applications/jetbrains-webstorm.desktop'
 sudo sh -c 'echo "Version=1.0" | tee -a /usr/share/applications/jetbrains-webstorm.desktop'
 sudo sh -c 'echo "Name=Webstorm" | tee -a /usr/share/applications/jetbrains-webstorm.desktop'
-sudo sh -c 'echo "Comment=IDE for web development" | tee -a /usr/share/applications/jetbrains-webstorm.desktop'
+sudo sh -c 'echo "Comment=ID for web developmentE" | tee -a /usr/share/applications/jetbrains-webstorm.desktop'
 sudo sh -c 'echo "Categories=Development;" | tee -a /usr/share/applications/jetbrains-webstorm.desktop'
 sudo sh -c 'echo "Exec=/usr/bin/webstorm" | tee -a /usr/share/applications/jetbrains-webstorm.desktop'
 sudo sh -c 'echo "Icon=/usr/share/pixmaps/webstorm.png" | tee -a /usr/share/applications/jetbrains-webstorm.desktop'
@@ -35,3 +35,25 @@ else
 echo "Webstorm: failed"
 exit 1
 fi
+
+
+echo "Installing webstorm..."
+# Create the /usr/share/pixmaps/ dir if it doesn't exist
+if [ ! -d /usr/share/pixmaps/ ]; then
+       sudo mkdir /usr/share/pixmaps/
+fi
+sudo cp /usr/bin/webstorm/bin/webstorm.png /usr/share/pixmaps/webstorm.png
+
+# Setup of the .desktop file
+sudo echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=WebStorm
+Comment=IDE for web development
+Categories=Development;
+Exec=/usr/bin/webstorm/bin/webstorm.sh
+Icon=/usr/share/pixmaps/webstorm.png
+Terminal=false" >> /usr/share/applications/webstorm.desktop
+xdg-desktop-menu install /usr/share/applications/webstorm.desktop
+
+echo "AgroUML has been installed"
